96daeb94779ed06f766c801f7cdeba0d6a090e34
wobetec <esdras2821@gmail.com>
2024-06-06 22:08:37 -0300

    all experiments

--- paper_experiments.py
+++ paper_experiments.py
@@ -2,41 +2,8 @@ from argparse import Namespace
 from src.experiments import BaseLine, Federated
 import torch
 from src.save import save_results
+import pandas as pd
 
-scheduled_experiments = [
-    {
-        'exp_name': 'baseline',
-
-        'epochs': 10,
-        'lr': 0.01,
-
-        'model': 'cnn',
-        'optimizer': 'sgd',
-
-        'dataset': 'mnist',
-        'num_classes': 10,
-        'iid': 1,
-        'unequal': 0,
-    },
-    # {
-    #     'exp': 'federated',
-
-    #     'epochs': '',
-    #     'num_users': '',
-    #     'frac': '',
-    #     'local_ep': '',
-    #     'local_bs': '',
-    #     'lr': '',
-
-    #     'model': '',
-    #     'optimizer': '',
-
-    #     'dataset': '',
-    #     'num_classes': '',
-    #     'iid': '',
-    #     'unequal': '',
-    # },
-]
 
 def run_experiment(experiment):
     experiment = Namespace(**experiment)
@@ -45,20 +12,23 @@ def run_experiment(experiment):
         exp.train()
     elif experiment.exp_name == 'federated':
         exp = Federated(experiment)
-        exp.train()   
+        exp.train()
     else:
         raise ValueError(f'Unrecognized experiment name: {experiment.exp_name}')
     save_results(exp)
 
 if __name__ == '__main__':
-
     if torch.cuda.is_available():
         print(f"GPU: {torch.cuda.get_device_name(0)} is available.")
-        gpu = None
+        gpu = 0
     else:
         print("No GPU available. Training will run on CPU.")
         gpu = None
-    for experiment in scheduled_experiments:
+
+    df = pd.read_csv('paper_models.csv')
+    scheduled_experiments = df.to_dict(orient='records')
+
+    for experiment in (scheduled_experiments):
         experiment['gpu'] = gpu
         experiment['verbose'] = 1
         run_experiment(experiment)
